309c7ad7aee78bc36a89597b92f54f6d4e794016
Thomega35 <thomega35@gmail.com>
2022-11-24 12:25:57 +0100

    ✨ Personalisation of the content in the template

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
+export const SITE_TITLE = 'Thomas Delapart Portfolio';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const LINKEDIN = 'https://www.linkedin.com/in/thomas-delapart-8aba0a222/';
+export const GITHUB = 'https://github.com/Thomega35/';
+export const EMAIL = 'thomega35@gmail.com';
+export const PHONE = '07 70 26 12 52';
