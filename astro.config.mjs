e70cb4c7c4c2a25a7d3b8bcf9393df94c24a4bdc
Thomega35 <thomega35@gmail.com>
2022-11-25 15:42:28 +0100

    ➕ Tailwind dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,12 @@ import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap(), react(), tailwind()],
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel()
 });
