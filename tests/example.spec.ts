bfb020ba3d391993dc6a67ab7f21bbb19529e2cc
Thomega35 <thomas.delapart@atos.net>
2023-03-02 10:56:35 +0100

    ✨Add Playwright to project with getting started

---
+++ tests/example.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects the URL to contain intro.
+  await expect(page).toHaveURL(/.*intro/);
+});
+
+test('contact navigation', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  await page.getByRole('link', { name: 'Contests' }).click();
+  await page.getByRole('link', { name: 'Personal projects' }).click();
+  await page.getByRole('link', { name: 'Contests' }).click();
+  
+  await expect(page).toHaveURL(/.*contest/);
+});
