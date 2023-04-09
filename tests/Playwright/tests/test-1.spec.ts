2248aa044a44c12f19c94ef46e7d6ee3c2136620
Thomega35 <thomas.delapart@atos.net>
2023-04-10 03:45:21 +0900

    ⚰️ Remove files in tests\ folder

--- tests/Playwright/tests/test-1.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://flowcv.me/thomas-delapart');
-  const page1Promise = page.waitForEvent('popup');
-  await page.getByRole('link').filter({ hasText: 'Linkedin icon' }).click();
-  const page1 = await page1Promise;
-  await expect(page1).toHaveURL(/.*linkedin/);
-});
