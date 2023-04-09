33dd93005ad2d125dd37699a16b2d5e6535b7254
Thomega35 <thomas.delapart@atos.net>
2023-04-10 05:43:13 +0900

    ⬆️ upgrade astro, tailwind, vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,16 @@ import vercel from '@astrojs/vercel/serverless';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://example.com/',
   integrations: [mdx(), sitemap(), react(), tailwind()],
   output: 'server',
   adapter: vercel()
